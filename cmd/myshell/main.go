04785f6c96fe8a9b0497bcf76999e079c376a3d3
Jayant70 <jayanthimanshu70@gmail.com>
2024-12-12 19:18:56 +0530

    imple exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -13,11 +14,17 @@ func main() {
 	// Uncomment this block to pass the first stage
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-
-		// Wait for user input
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-
-		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		cmd = strings.TrimSpace(cmd)
+		switch cmd {
+		case "exit 0":
+			os.Exit(0)
+		default:
+			fmt.Printf("%s: command not found\n", cmd)
+		}
 	}
-	os.Exit(0)
 }
